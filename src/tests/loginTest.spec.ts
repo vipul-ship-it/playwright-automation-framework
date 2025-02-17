91bd5faf1de9a798d06cc1c72d58f5929a35e917
vipul <vipulkhebade.60@gmail.com>
2025-02-17 12:19:38 +0530

    adding cryptoJS utility

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 //import logger from "../utils/LoggerUtil";
-//import { decrypt } from "../utils/CryptojsUtil";
+import {encrypt, decrypt } from "../utils/CryptoJsUtils";
+import { encryptEnvFile, decryptEnvFile } from "../utils/EncryptEnvFile";
 
 const authFile = "src/config/auth.json";
 
-test("simple login test with self heal", async ({ page }) => {
+test.skip("simple login test with self heal", async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.navigateToLoginPage();
   //await loginPage.fillUsername_selfheal("demo_selfheal");
@@ -14,16 +15,16 @@ test("simple login test with self heal", async ({ page }) => {
 test("simple login test", async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.navigateToLoginPage();
-  await loginPage.fillUsername(process.env.userid!);//! is for not null value in TS 
-  await loginPage.fillPassword(process.env.password!);//process.env is used in Node.jsto access environment variables, which are key-value pairs
+  await loginPage.fillUsername(decrypt(process.env.userid!));//! is for not null value in TS 
+  await loginPage.fillPassword(decrypt(process.env.password!));//process.env is used in Node.jsto access environment variables, which are key-value pairs
   
   const homePage = await loginPage.clickLoginButton();
   await homePage.expectServiceTitleToBeVisible();
   //logger.info("Test for login is completed");
-  await page.context().storageState({ path: authFile });
+ // await page.context().storageState({ path: authFile });
   //logger.info("Auth state is saved");
 });
-test("sample env test", async({page})=>{
+test.skip("sample env test", async({page})=>{
   console.log(process.env.NODE_ENV);
   console.log(process.env.userid);
   console.log(process.env.password);
@@ -37,3 +38,14 @@ test("sample env test", async({page})=>{
 //   );
 //   await expect(page.getByRole("link", { name: "Accounts" })).toBeVisible();
 // });
+test.skip("sample env test ", async({page})=>{
+  // const plainText = "Hello, mars!";
+  // const encryptedText = encrypt(plainText);
+  // console.log('SALT:', process.env.SALT);
+  // console.log('Encrypted:', encryptedText);
+  // const decryptedText = decrypt(encryptedText);
+  // console.log('Decrypted:',decryptedText)
+
+  encryptEnvFile();
+
+})
